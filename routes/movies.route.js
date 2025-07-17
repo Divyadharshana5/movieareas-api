c53db1bba22dd7efdb56644333f9671f2e744b9b
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-07-17 16:31:43 +0530

    Implement response handling for movie routes in movies.route.js

--- routes/movies.route.js
+++ routes/movies.route.js
@@ -2,15 +2,23 @@ import express from "express";
 const router = express.Router();
 
 //R-For Read
-router.get("/movies", () => {});
+router.get("/movies", (req, res) => {
+  res.send("Get all movies");
+});
 
 //C-For Create
-router.post("/movies", () => {});
+router.post("/movies", (req, res) => {
+  res.send("Create movies");
+});
 
 //U-For Update
-router.put("/movies/:id", () => {});
+router.put("/movies/:id", (req, res) => {
+  res.send("updates movies");
+});
 
 //D-For Delete
-router.delete("/movies/:id", () => {});
+router.delete("/movies/:id", (req, res) => {
+  res.send("Delete all movies");
+});
 
 export default router;
