81d55b465fbc5b444867322c98fb22a512a9c195
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-07-18 12:12:00 +0530

    refactor: export movie controller functions and update routes to use them

--- controllers/movies.controller.js
+++ controllers/movies.controller.js
@@ -1,3 +1,15 @@
-const MovieIndex = (req, res) => {
+export const MovieIndex = (req, res) => {
   res.send("Get all movies");
+};
+
+export const MovieCreate = (req, res) => {
+  res.send("Create movies");
+};
+
+export const MovieUpdate = (req, res) => {
+  res.send("updates movies");
+};
+
+export const MovieDelete = (req, res) => {
+  res.send("Delete all movies");
 };
--- routes/movies.route.js
+++ routes/movies.route.js
@@ -2,21 +2,15 @@ import express from "express";
 const router = express.Router();
 
 //R-For Read
-router.get("/");
+router.get("/", MovieIndex);
 
 //C-For Create
-router.post("/", (req, res) => {
-  res.send("Create movies");
-});
+router.post("/", MovieCreate);
 
 //U-For Update
-router.put("/:id", (req, res) => {
-  res.send("updates movies");
-});
+router.put("/:id", MovieUpdate);
 
 //D-For Delete
-router.delete("/:id", (req, res) => {
-  res.send("Delete all movies");
-});
+router.delete("/:id", MovieDelete);
 
 export default router;
