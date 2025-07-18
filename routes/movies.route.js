42741e690d91389c442d9da03910af405cc081a2
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-07-18 12:00:27 +0530

    fix: update movie routes to use root path instead of '/movies'

--- routes/movies.route.js
+++ routes/movies.route.js
@@ -2,22 +2,22 @@ import express from "express";
 const router = express.Router();
 
 //R-For Read
-router.get("/movies", (req, res) => {
+router.get("/", (req, res) => {
   res.send("Get all movies");
 });
 
 //C-For Create
-router.post("/movies", (req, res) => {
+router.post("/", (req, res) => {
   res.send("Create movies");
 });
 
 //U-For Update
-router.put("/movies/:id", (req, res) => {
+router.put("/:id", (req, res) => {
   res.send("updates movies");
 });
 
 //D-For Delete
-router.delete("/movies/:id", (req, res) => {
+router.delete("/:id", (req, res) => {
   res.send("Delete all movies");
 });
 
